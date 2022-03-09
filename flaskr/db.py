de01ca2b52bcec705128803a8bc15dde9b156505
LucaDelBasso <lucadelbasso98@gmail.com>
2022-03-09 22:41:06 +0000

    click command for db init

--- flaskr/db.py
+++ flaskr/db.py
@@ -42,8 +42,33 @@ def close_db(e=None):
       along, application gets told about close_db in the app factory
       so that it is called after each request.
     '''
-    
+
     db = g.pop('db', None)
 
     if db is not None:
         db.close()
+
+def init_db():
+    db = get_db()
+
+    ''' 
+       open_resource opens a file relative to the flaskr package.
+       useful as this location isn't always known when deploying elsewhere.
+       get_db returns a database connection, which is used to execute
+       the commands read from the file.
+    '''
+
+    with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+
+'''
+    click.command() definds a command line command called init-db
+    that calls the init_db function and shows a success message to
+    the user.
+'''
+@click.command('init-db')
+@with_appcontext
+def init_db_command():
+    '''Clear the existing data and create new tables'''
+    init_db()
+    click.echo('Initialized the database.')
