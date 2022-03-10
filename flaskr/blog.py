6f71755abba9d2fa9048608e5f3cefc418841ba9
LucaDelBasso <lucadelbasso98@gmail.com>
2022-03-10 22:27:59 +0000

    small bug fixes

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -15,11 +15,10 @@ def index():
     db = get_db()
     posts = db.execute(
         'SELECT p.id, title, body, created, author_id, username'
-         'FROM post p JOIN user u ON p.author_id = u.id'
-         'ORDER BY created DESC'
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' ORDER BY created DESC'
     ).fetchall()
     return render_template('blog/index.html', posts=posts)
-
 
 @bp.route('/create', methods=('GET', 'POST'))
 @login_required
@@ -38,7 +37,7 @@ def create():
             db = get_db()
             db.execute(
                 'INSERT INTO post (title, body, author_id)'
-                 'VALUES (?, ?, ?)',
+                ' VALUES (?, ?, ?)',
                  (title, body, g.user['id'])
             )
             db.commit()
@@ -55,8 +54,8 @@ def get_post(id, check_author=True):
     '''
     post = get_db().execute(
         'SELECT p.id, title, body, created, author_id, username'
-         'FROM post p JOIN user u ON p.author_id = u.id'
-         'WHERE p.id = ?',
+        ' FROM post p JOIN user u ON p.author_id = u.id'
+        ' WHERE p.id = ?',
          (id,)
     ).fetchone()
 
@@ -94,7 +93,7 @@ def update(id):
             db = get_db()
             db.execute(
                 'UPDATE post SET title = ?, body = ?'
-                 ' WHERE id = ?',
+                ' WHERE id = ?',
                  (title, body, id)
             )
             db.commit()
@@ -111,3 +110,5 @@ def delete(id):
     db.execute('DELETE FROM post WHERE id = ?', (id,))
     db.commit()
     return redirect(url_for('blog.index'))
+
+    
