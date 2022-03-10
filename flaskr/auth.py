93a74099dc01be95884d9f30ebce4196c641f45d
LucaDelBasso <lucadelbasso98@gmail.com>
2022-03-10 13:22:56 +0000

    authentication blueprint

---
+++ flaskr/auth.py
@@ -0,0 +1,20 @@
+import functools
+
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request,
+    session, url_for
+)
+
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from flaskr.db import get_db
+
+bp = Blueprint('auth', __name__ , url_prefix='/auth')
+
+
+'''
+    This creates a Blueprint named 'auth'. Like the application obj, the
+    blueprint needs to know where it is defined, so __name__ is passed as
+    the second argument. the url_prefix will be prepended to all the URLs
+    associated with the blueprint.
+'''
