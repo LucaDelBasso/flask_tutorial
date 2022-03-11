32716137d756075f1bc99b4c471905f30d469bd3
LucaDelBasso <lucadelbasso98@gmail.com>
2022-03-11 08:14:54 +0000

    notes in setup and tests init

--- flaskr/setup.py
+++ flaskr/setup.py
@@ -10,3 +10,14 @@ setup(
         'flask',
     ],
 )
+
+'''
+    packages tells python what package directories
+    and their python files to include.
+    find_packages() finds these directories automatically
+    so you don't have to type them out.
+    To include other files, like the static ones and template
+    directories, include_package_data is set.
+
+    MANIFEST.in is used to tell what this other data is.
+'''
