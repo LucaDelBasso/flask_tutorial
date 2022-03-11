fe8eae57e0405a6e8262082e0314640259cf978c
LucaDelBasso <lucadelbasso98@gmail.com>
2022-03-11 13:53:29 +0000

    tests for test_db

---
+++ tests/test_db.py
@@ -0,0 +1,40 @@
+import sqlite3
+
+import pytest
+from flaskr.db import get_db
+
+
+''' 
+    Within an application context, get_db should return the same connection each time it's called.
+    After the context, the connection should be closed.
+'''
+def test_get_close_db(app):
+    with app.app_context():
+        db = get_db()
+        assert db is get_db()
+
+    # this tests that the db has closed after use in the app context
+    with pytest.raises(sqlite3.ProgrammingError) as e:
+        db.execute('SELECT 1')
+
+    assert 'closed' in str(e.value)
+
+
+#the init-db command should call the init-db function and output a message
+def test_init_db_command(runner, monkeypatch):
+    ''' 
+        This test uses Pytest's monkeypatch fixture to replace the init_db function
+        with one that records that it's been called.
+
+        The runner fixture in conftest.py is used to call init-db command by name.
+    '''
+    class Recorder(object):
+        called = False
+
+    def fake_init_db():
+        Recorder.called = True
+
+    monkeypatch.setattr('flaskr.db.init_db', fake_init_db)
+    result = runner.invoke(args=['init-db'])
+    assert 'Initialized' in result.output
+    assert Recorder.called
